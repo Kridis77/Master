ae02ecd6c0f2eb27c5e5628dbd6e542d605c1677
Rokas Kazlauskas <rokas.kazlauskas@rewell.se>
2019-11-19 20:40:51 +0200

    initial

---
+++ Master/Master/Controllers/BankController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Master.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BankController : ControllerBase
+    {
+        [Route("balance")]
+        [HttpGet]
+        public double Balance()
+        {
+            return Vault.TheBank.GetBalance();
+        }
+
+        [Route("add/{money}")]
+        [HttpGet]
+        public void Add(double money)
+        {
+            Vault.TheBank.AddMoney(money);
+        }
+
+        [Route("remove/{money}")]
+        [HttpGet]
+        public void Remove(double money)
+        {
+            Vault.TheBank.RemoveMoney(money);
+        }
+
+    }
+    public static class Vault
+    {
+        public static Bankas TheBank = new Bankas();
+    }
+}
---
+++ Master/Master/Controllers/Bankas.cs
@@ -0,0 +1,42 @@
+﻿namespace Master.Controllers
+{
+    public class Bankas
+    {
+        public Bankas()
+        {
+            Mokesciai = 0.02;
+            DraugiskesniMokesciai = 0.005;
+            ArSumoketa = true;
+        }
+        private double Mokesciai { get; set; }
+        private double DraugiskesniMokesciai { get; set; }
+        private double Pinigai { get; set; }
+        private bool ArSumoketa { get; set; }
+
+        public void AddMoney(double money)
+        {
+            var mokestis = money * Mokesciai;
+            if (!ArSumoketa)
+            {
+                return;
+            }
+            Pinigai += money;
+            Pinigai -= mokestis;
+        }
+        public void RemoveMoney(double money)
+        {
+            var geriMokesciai = money * DraugiskesniMokesciai;
+            if (Pinigai > money)
+            {
+                Pinigai -= geriMokesciai;
+                Pinigai -= money;
+            }
+           
+        }
+
+        public double GetBalance()
+        {
+            return Pinigai;
+        }
+    }
+}
